31596961996dcc9678f97945415a5d50be9e86d1
irvin <irvin.avalos213@gmail.com>
2026-01-09 00:40:23 -0800

    initial tokenizer

---
+++ lib/token.ml
@@ -0,0 +1,38 @@
+type token_type =
+  | ILLEGAL
+  | EOF
+  | IDENT
+  | INT
+  | ASSIGN
+  | PLUS
+  | COMMA
+  | SEMICOLON
+  | LPAREN
+  | RPAREN
+  | LBRACE
+  | RBRACE
+  | FUNCTION
+  | LET
+
+type token = { typ : token_type; literal : string }
+
+let string_of_token_type (tt : token_type) : string =
+  match tt with
+  | ILLEGAL -> "ILLEGAL"
+  | EOF -> "EOF"
+  (* Identifies and Literals *)
+  | IDENT -> "IDENT"
+  | INT -> "INT"
+  (* Operators *)
+  | ASSIGN -> "ASSIGN"
+  | PLUS -> "PLUS"
+  (* Delimiters *)
+  | COMMA -> "COMMA"
+  | SEMICOLON -> "SEMICOLON"
+  | LPAREN -> "LPAREN"
+  | RPAREN -> "RPAREN"
+  | LBRACE -> "LBRACE"
+  | RBRACE -> "RBRACE"
+  (* Keywords *)
+  | FUNCTION -> "FUNCTION"
+  | LET -> "LET"
