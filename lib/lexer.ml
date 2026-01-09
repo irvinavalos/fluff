62ae3b53a41bcb3773d1057712c98b3806e01638
irvin <irvin.avalos213@gmail.com>
2026-01-09 04:44:12 -0800

    added functions for reading letters and identifiers

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -17,6 +17,28 @@ let read_char (l : lexer) : lexer =
     ch;
   }
 
+let is_char_valid c =
+  ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_'
+
+let is_letter (ch : char option) : bool =
+  Option.fold ~none:false ~some:(fun c -> is_char_valid c) ch
+
+let rec read_identifier_acc l acc =
+  if is_letter l.ch then read_identifier_acc (read_char l) (acc + 1) else acc
+
+let read_identifer_helper l = read_identifier_acc l l.position
+
+let read_identifier (l : lexer) : lexer * string =
+  let old_position = l.position in
+  let new_position = read_identifer_helper l in
+  ( {
+      input = l.input;
+      position = new_position;
+      read_position = new_position + 1;
+      ch = Some l.input.[new_position];
+    },
+    String.sub l.input old_position new_position )
+
 let new_lexer (input : string) : lexer =
   read_char { input; position = 0; read_position = 0; ch = None }
 
