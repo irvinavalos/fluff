20b2616ffe9066a7439ab4faf19262cadf4e3dde
irvin <irvin.avalos213@gmail.com>
2026-01-09 03:49:04 -0800

    halfway done with lexer (i think), added next_token function

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -25,3 +25,27 @@ let new_token (tt : Token.token_type) (ch : char option) : Token.token =
     typ = tt;
     literal = Option.fold ~none:"" ~some:(fun c -> String.make 1 c) ch;
   }
+
+let get_char_from_char_opt ch = Option.get ch
+
+let helper (ch : char option) =
+  let c = get_char_from_char_opt ch in
+  match c with
+  | '{' -> Token.LBRACE
+  | '}' -> Token.RBRACE
+  | '(' -> Token.LPAREN
+  | ')' -> Token.RPAREN
+  | ';' -> Token.SEMICOLON
+  | ',' -> Token.COMMA
+  | '+' -> Token.PLUS
+  | '=' -> Token.ASSIGN
+  | _ -> Token.EOF
+
+let next_token_helper (ch : char option) =
+  let tt = helper ch in
+  new_token tt ch
+
+let next_token (l : lexer) : lexer * Token.token =
+  let return_token = next_token_helper l.ch in
+  let return_lexer = read_char l in
+  (return_lexer, return_token)
