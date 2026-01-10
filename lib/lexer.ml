364255d870d17c0b00b7a8aa76e7aaefdbc18ec0
irvin <irvin.avalos213@gmail.com>
2026-01-09 18:59:43 -0800

    implemented read_identifier function into next_token function

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -18,56 +18,47 @@ let read_char (l : lexer) : lexer =
   }
 
 let is_char_valid c =
-  ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_'
+  ("a" <= c && c <= "z") || ("A" <= c && c <= "Z") || c == "_"
 
-let is_letter (ch : char option) : bool =
-  Option.fold ~none:false ~some:(fun c -> is_char_valid c) ch
+let is_letter (ch : string) : bool = is_char_valid ch
 
 let rec read_identifier_acc l acc =
-  if is_letter l.ch then read_identifier_acc (read_char l) (acc + 1) else acc
+  if is_letter (Option.fold ~none:"" ~some:(fun x -> String.make 1 x) l.ch) then
+    read_identifier_acc (read_char l) (acc + 1)
+  else acc
 
 let read_identifer_helper l = read_identifier_acc l l.position
 
-let read_identifier (l : lexer) : lexer * string =
+let read_identifier (l : lexer) : string =
   let old_position = l.position in
   let new_position = read_identifer_helper l in
-  ( {
-      input = l.input;
-      position = new_position;
-      read_position = new_position + 1;
-      ch = Some l.input.[new_position];
-    },
-    String.sub l.input old_position new_position )
+  String.sub l.input old_position new_position
 
 let new_lexer (input : string) : lexer =
   read_char { input; position = 0; read_position = 0; ch = None }
 
-let new_token (tt : Token.token_type) (ch : char option) : Token.token =
-  {
-    typ = tt;
-    literal = Option.fold ~none:"" ~some:(fun c -> String.make 1 c) ch;
-  }
+let new_token (tt : Token.token_type) (ch : string) : Token.token =
+  { typ = tt; literal = ch }
 
 let get_char_from_char_opt ch = Option.get ch
 
-(* TODO: implement read_identifier and is_letter into next_token function *)
-let next_token_helper ch =
-  let tt =
-    let c = get_char_from_char_opt ch in
-    match c with
-    | '{' -> Token.LBRACE
-    | '}' -> Token.RBRACE
-    | '(' -> Token.LPAREN
-    | ')' -> Token.RPAREN
-    | ';' -> Token.SEMICOLON
-    | ',' -> Token.COMMA
-    | '+' -> Token.PLUS
-    | '=' -> Token.ASSIGN
-    | _ -> Token.EOF
-  in
-  new_token tt ch
+let next_token_helper l =
+  let c = Option.fold ~none:"" ~some:(fun x -> String.make 1 x) l.ch in
+  match c with
+  | "{" -> new_token Token.LBRACE c
+  | "}" -> new_token Token.RBRACE c
+  | "(" -> new_token Token.LPAREN c
+  | ")" -> new_token Token.RPAREN c
+  | ";" -> new_token Token.SEMICOLON c
+  | "," -> new_token Token.COMMA c
+  | "+" -> new_token Token.PLUS c
+  | "=" -> new_token Token.ASSIGN c
+  | "" -> new_token Token.EOF c
+  | _ ->
+      if is_letter c then new_token (Token.lookup_ident c) (read_identifier l)
+      else new_token Token.ILLEGAL c
 
 let next_token (l : lexer) : lexer * Token.token =
-  let return_token = next_token_helper l.ch in
+  let return_token = next_token_helper l in
   let return_lexer = read_char l in
   (return_lexer, return_token)
